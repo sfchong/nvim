8755e1123420a52d2e6453d70f1a403af0452bb7
Adrian Chong <adrianchong@seekasia.com>
2024-02-21 21:32:12 +0800

    disable treesitter for large file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,17 @@ return {
     { 'windwp/nvim-ts-autotag' }
   },
   opts = {
-    highlight = { enable = true },
+    highlight = {
+      enable = true,
+      -- disable treesitter for large files
+      disable = function(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    },
     indent = { enable = false },
     ensure_installed = {
       'bash',
