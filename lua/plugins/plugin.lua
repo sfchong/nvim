67d168af3682d7abe7789ad90fd771438912efd8
Adrian Chong <adrianchong@seekasia.com>
2024-03-01 19:08:03 +0800

    add copilot

--- lua/plugins/plugin.lua
+++ lua/plugins/plugin.lua
@@ -1,24 +1,37 @@
 return {
   {
     'windwp/nvim-autopairs',
-    event = "InsertEnter",
+    event = 'InsertEnter',
     opts = {} -- this is equalent to setup({}) function
   },
   {
-    'karb94/neoscroll.nvim',
-    config = function()
-      require('neoscroll').setup({
-        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
-          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-      })
-    end
-  },
-  {
     'kylechui/nvim-surround',
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
     event = 'VeryLazy',
     config = function()
       require('nvim-surround').setup()
     end
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = '<Tab>',
+            accept_word = false,
+            accept_line = false,
+            next = "<F8>",
+            prev = "<F7>",
+            dismiss = "<F9>",
+          },
+        },
+      })
+    end,
   }
 }
