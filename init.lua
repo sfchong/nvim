192325bb030860cffc2997a840c11d1ef019fc11
Adrian Chong <adrianchong@seekasia.com>
2024-03-19 20:58:43 +0800

    add vscode support

--- init.lua
+++ init.lua
@@ -1,27 +1,32 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
+if vim.g.vscode then
+  vim.g.mapleader = ' '
+  require('options')
+  require('keymap')
+else
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  -- use space as leader key
+  vim.g.mapleader = ' '
+
+  require("lazy").setup('plugins', {
+    change_detection = {
+      enabled = true,
+      notify = false,
+    },
   })
+
+  require('options')
+  require('keymap')
+  require('autocmd')
 end
-vim.opt.rtp:prepend(lazypath)
-
--- use space as leader key
-vim.g.mapleader = ' '
-
-require("lazy").setup('plugins', {
-  change_detection = {
-    enabled = true,
-		notify = false,
-	},
-})
-
-require('options')
-require('keymap')
-require('autocmd')
-
