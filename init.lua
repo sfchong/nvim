3f632f6616fa9c65e3bf596ca8fa1222af8a1bb2
Adrian Chong <adrian@adrianchongsf.com>
2024-03-20 02:18:41 +0800

    add vscode keymap

--- init.lua
+++ init.lua
@@ -1,7 +1,44 @@
 if vim.g.vscode then
+  -- use space as leader key
   vim.g.mapleader = ' '
-  require('options')
-  require('keymap')
+
+  local vscode = require('vscode-neovim')
+  local function map_vscode(mode, keymap, action, opts)
+    vim.keymap.set(mode, keymap, function()
+      vscode.call(action, opts)
+    end)
+  end
+
+  -- clipboard related
+  vim.keymap.set('x', 'p', '\"_dP')
+  vim.keymap.set('n', 'x', '\"_x')
+  vim.keymap.set('v', 'x', '\"_x')
+  vim.keymap.set('n', 'c', '\"_c')
+
+  -- keep visual mode during indent
+  vim.keymap.set('v', '<', '<gv')
+  vim.keymap.set('v', '>', '>gv')
+
+  -- editor action
+  map_vscode('n', '<leader>s', 'workbench.action.files.save')
+  map_vscode('n', '<leader>a', 'editor.action.selectAll')
+  map_vscode({ 'n', 'v' }, '<leader>/', 'editor.action.commentLine')
+  map_vscode('n', 'U', 'redo')
+
+  -- TODO: make this work
+  vim.keymap.set('n', '<leader>w', '<C-w>')
+
+  -- navigation
+  map_vscode('n', '<leader> ', 'workbench.action.quickOpen')
+  map_vscode('n', '<leader>fn', 'actions.find')
+  map_vscode('n', '<leader>fg', 'workbench.action.findInFiles')
+  map_vscode('n', '<leader>fG', 'search.action.openEditor')
+  map_vscode('n', '<leader>b', 'workbench.action.showAllEditors')
+  map_vscode('n', '<leader>,', 'workbench.action.navigateToLastNavigationLocation')
+
+  -- lsp action
+  map_vscode('n', '<leader>.', 'editor.action.quickFix')
+  map_vscode('n', 'gr', 'editor.action.referenceSearch.trigger')
 else
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
   if not vim.loop.fs_stat(lazypath) then
