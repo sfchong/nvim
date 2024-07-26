4a61f1c17ffee4333a57080ae1d1e9e9b1049909
Adrian Chong <adrianchong@seekasia.com>
2024-07-26 14:18:31 +0800

    fix conform deprecated config

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -2,32 +2,39 @@ return {
   'stevearc/conform.nvim',
   dependencies = { 'mason.nvim' },
   event = 'LspAttach',
-  opts = {
-    formatters_by_ft = {
-      typescript = { { 'prettierd', 'prettier' } },
-      typescriptreact = { { 'prettierd', 'prettier' } },
-      javascript = { { 'prettierd', 'prettier' } },
-      javascriptreact = { { 'prettierd', 'prettier' } },
-      json = { { 'prettierd', 'prettier' } },
-      jsonc = { { 'prettierd', 'prettier' } },
-      html = { { 'prettierd', 'prettier' } },
-      css = { { 'prettierd', 'prettier' } },
-      scss = { { 'prettierd', 'prettier' } },
-      markdown = { { 'prettierd', 'prettier' } },
-      yaml = { { 'prettierd', 'prettier' } },
-      go = { 'gofmt' }
-    },
-    format_on_save = {
-      lsp_fallback = true,
-      timeout_ms = 500,
-    },
-    format_after_save = {
-      lsp_fallback = true,
-    },
-  },
-  config = function(_, opts)
+  config = function()
+    local prettier_formatter = {
+      'prettierd',
+      'prettier',
+      stop_after_first = true
+    }
+
+    local options = {
+      formatters_by_ft = {
+        typescript = prettier_formatter,
+        typescriptreact = prettier_formatter,
+        javascript = prettier_formatter,
+        javascriptreact = prettier_formatter,
+        json = prettier_formatter,
+        jsonc = prettier_formatter,
+        html = prettier_formatter,
+        css = prettier_formatter,
+        scss = prettier_formatter,
+        markdown = prettier_formatter,
+        yaml = prettier_formatter,
+        go = { 'gofmt' }
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 500,
+      },
+      format_after_save = {
+        lsp_fallback = true,
+      },
+    }
+
     local conform = require('conform')
-    conform.setup(opts)
+    conform.setup(options)
 
     vim.keymap.set({ 'n', 'v' }, '<leader>cf', function()
       conform.format({ lsp_fallback = true, async = true, timeout_ms = 500 })
