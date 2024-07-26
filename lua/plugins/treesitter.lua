8b19be234593e08e4ebaaba9e86441c2d6bcef65
Adrian Chong <adrianchong@seekasia.com>
2024-07-26 13:17:34 +0800

    fix nvim-ts-autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,50 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  event = 'VeryLazy',
-  dependencies = {
-    { 'windwp/nvim-ts-autotag' }
-  },
-  opts = {
-    highlight = {
-      enable = true,
-      -- disable treesitter for large files
-      disable = function(_, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
-      end,
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = 'VeryLazy',
+    opts = {
+      highlight = {
+        enable = true,
+        -- disable treesitter for large files
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+      indent = { enable = false },
+      ensure_installed = {
+        'bash',
+        'css',
+        'c_sharp',
+        'go',
+        'html',
+        'javascript',
+        'json',
+        'jsonc',
+        'lua',
+        'markdown',
+        'markdown_inline',
+        'scss',
+        'svelte',
+        'toml',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc'
+      },
     },
-    indent = { enable = false },
-    ensure_installed = {
-      'bash',
-      'css',
-      'c_sharp',
-      'go',
-      'html',
-      'javascript',
-      'json',
-      'jsonc',
-      'lua',
-      'markdown',
-      'markdown_inline',
-      'scss',
-      'svelte',
-      'toml',
-      'tsx',
-      'typescript',
-      'vim',
-      'vimdoc'
-    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end,
   },
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-  end,
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  }
 }
