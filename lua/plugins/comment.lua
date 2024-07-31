3d7b621a79ee68947d3fa948e2c196cdce809212
Adrian Chong <adrianchong@seekasia.com>
2024-07-31 16:16:05 +0800

    enable comment on jsx/tsx

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,22 +1,23 @@
 return {
   'numToStr/Comment.nvim',
-  config = function ()
+  dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+  config = function()
     local comment = require('Comment')
     comment.setup({
-    toggler = {
+      toggler = {
         ---Line-comment toggle keymap
         line = '<leader>/',
         ---Block-comment toggle keymap
         block = 'gbc',
-    },
-    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-    opleader = {
+      },
+      ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+      opleader = {
         ---Line-comment keymap
         line = '<leader>/',
         ---Block-comment keymap
         block = 'gb',
-    }
-  })
-
+      },
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+    })
   end
 }
