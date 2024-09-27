4e4e8dea76bfc459dcf86cff17e9af093686f71b
Adrian Chong <adrianchong@seekasia.com>
2024-09-27 10:45:03 +0800

    add more window key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -10,12 +10,17 @@ return {
     local wk = require('which-key')
 
     wk.add({
-      { "<leader>b", group = "Buffer actions" },
-      { "<leader>c", group = "Code actions" },
-      { "<leader>e", group = "File explorer" },
-      { "<leader>f", group = "Find with Telescope" },
-      { "<leader>p", group = "Harpoon" },
-      { "<leader>w", proxy = "<C-w>",              desc = "Window actions" },
+      { "<leader>b",    group = "Buffer actions" },
+      { "<leader>c",    group = "Code actions" },
+      { "<leader>e",    group = "File explorer" },
+      { "<leader>f",    group = "Find with Telescope" },
+      { "<leader>p",    group = "Harpoon" },
+      { "<leader>w",    proxy = "<C-w>",                desc = "Window actions" },
+      { "<c-w>c",       desc = "Quit a window" },
+      { "<c-w><left>",  desc = "Go to the left window" },
+      { "<c-w><right>", desc = "Go to the right window" },
+      { "<c-w><up>",    desc = "Go to the up window" },
+      { "<c-w><down>",  desc = "Go to the down window" },
     })
   end
 }
