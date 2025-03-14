ea6a55173388bd913d085f2bb7c073c46f3647de
Adrian Chong <adrianchong@seekasia.com>
2025-03-14 16:38:03 +0800

    add oil and markview

--- lua/plugins/plugin.lua
+++ lua/plugins/plugin.lua
@@ -13,6 +13,29 @@ return {
     end
   },
   {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    config = function()
+      local oil = require('oil')
+      oil.setup({
+        keymaps = {
+          ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
+          ['q'] = { 'actions.close', mode = 'n' },
+          ['<left>'] = { 'actions.parent', mode = 'n' },
+          ['<right>'] = { 'actions.select', mode = 'n' }
+        }
+      })
+
+      vim.keymap.set('n', '<leader>eo', '<cmd>Oil --float<CR>', { desc = 'Oil Explorer' })
+    end
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false
+  },
+  {
     'github/copilot.vim',
     event = 'InsertEnter'
   }
