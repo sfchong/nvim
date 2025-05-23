e88cd316ae1c9765c2d22242f757d043f0e766e5
Adrian Chong <adrianchong@seekasia.com>
2025-05-23 13:09:56 +0800

    update neovim to 0.11 and fix config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,13 +10,10 @@ return {
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'folke/neodev.nvim' },
     { 'nvim-telescope/telescope.nvim' },
-    { 'Hoffs/omnisharp-extended-lsp.nvim' }
   },
   config = function()
     require('neodev').setup({})
 
-    local cmp_nvim_lsp = require('cmp_nvim_lsp')
-    local lspconfig = require('lspconfig')
     local mason_lspconfig = require('mason-lspconfig')
     local telescope = require('telescope.builtin')
 
@@ -30,7 +27,6 @@ return {
       desc = 'LSP actions',
       callback = function(args)
         vim.bo[args.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
 
         local map = function(mode, lhs, rhs, desc)
           vim.keymap.set(mode, lhs, rhs, { buffer = args.buf, desc = desc })
@@ -67,7 +63,8 @@ return {
         end
 
         map('n', 'K', vim.lsp.buf.hover, 'Hover')
-        map('n', 'gd', function() telescope.lsp_definitions() end, 'Go to definition')
+        -- map('n', 'gd', function() telescope.lsp_definitions() end, 'Go to definition')
+        map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
         map('n', 'gD', vim.lsp.buf.declaration, 'Go to declaration')
         map('n', 'gI', function() telescope.lsp_implementations() end, 'Go to implementation')
         map('n', 'go', function() telescope.lsp_type_definitions() end, 'Go to type definition')
@@ -81,24 +78,43 @@ return {
         map('n', '<leader>.', vim.lsp.buf.code_action, 'Code action')
         map('n', '<leader>ce', vim.diagnostic.open_float, 'Open float')
         map('n', '<leader>ct', toggle_unit_test, 'Toggle unit test')
-
-        if client.name == 'omnisharp' then
-          map('n', 'gd', function() require('omnisharp_extended').telescope_lsp_definitions() end,
-            'Go to definition (omnisharp)')
-        end
-        if client.name == 'eslint' then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = args.buf,
-            command = "EslintFixAll",
-          })
-        end
       end
     })
 
+    vim.lsp.config('lua_ls', {
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = vim.split(package.path, ';'),
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { 'vim' },
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file('', true),
+            checkThirdParty = false
+          },
+        },
+      },
+    })
 
-    local lsp_options = {
-      capabilities = cmp_nvim_lsp.default_capabilities()
-    }
+    local base_on_attach = vim.lsp.config.eslint.on_attach
+    vim.lsp.config("eslint", {
+      on_attach = function(client, bufnr)
+        if not base_on_attach then return end
+
+        base_on_attach(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          command = "LspEslintFixAll",
+        })
+      end,
+    })
 
     mason_lspconfig.setup({
       ensure_installed = {
@@ -109,47 +125,6 @@ return {
         'eslint',
         'graphql'
       }
-    })
-
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup(lsp_options)
-      end,
-      ['lua_ls'] = function()
-        lspconfig.lua_ls.setup(vim.tbl_deep_extend('force', lsp_options, {
-          cmd = { 'lua-language-server' },
-          settings = {
-            Lua = {
-              runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = vim.split(package.path, ';'),
-              },
-              diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-              },
-              workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file('', true),
-                checkThirdParty = false
-              },
-            },
-          },
-        }))
-      end,
-      ["omnisharp"] = function()
-        lspconfig.omnisharp.setup(vim.tbl_deep_extend("force", lsp_options, {
-          handlers = {
-            ["textDocument/definition"] = require('omnisharp_extended').handler,
-          },
-          cmd = {
-            "dotnet",
-            vim.fn.expand("$HOME/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll"),
-          },
-        }))
-      end
     })
   end
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
+  commit = "b4da76be54691e854d3e0e02c36b0245f945c2c7",
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
     { 'nvim-telescope/telescope-file-browser.nvim' },
